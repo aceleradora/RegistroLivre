23751e04a1fc78f76de518c42a3b9a55abb7d73f
AlexandreMorales <lrcezimbra@hotmail.com>
2014-11-04 10:02:42 -0200

    Initial Commit

---
+++ src/main/java/br/com/aceleradora/RegistroLivre/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.aceleradora.RegistroLivre.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class HomeController {
+	
+	@Get("/")
+	public void home() {
+		System.out.println("hello world");
+	}
+
+}
