a385b948e599dc1fc7e4589298765cd9d5e12921
Henrique Braga <hbraga.santos@gmail.com>
2014-11-07 11:37:04 -0200

    2:Joseane e Henrique - Criação do Model e Teste do Sócio

---
+++ src/main/java/br/com/aceleradora/RegistroLivre/test/SocioTest.java
@@ -0,0 +1,27 @@
+package br.com.aceleradora.RegistroLivre.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.aceleradora.RegistroLivre.model.Socio;
+
+public class SocioTest {
+
+	Socio socio;
+	
+	@Before
+	public void setUp(){
+		socio = new Socio();
+	}
+	
+	@Test
+	public void testaSeCpfEValido() throws Exception {
+		
+		
+	}
+
+	
+	
+}
