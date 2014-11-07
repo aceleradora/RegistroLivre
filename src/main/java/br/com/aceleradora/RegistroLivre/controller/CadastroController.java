158e9c16ba4067f58aa42c095a78fcb926ff27c1
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-07 13:55:44 -0200

    #1:<Jordani/Everton>-atualizado Cadastro.jsp

--- src/main/java/br/com/aceleradora/RegistroLivre/controller/CadastroController.java
+++ src/main/java/br/com/aceleradora/RegistroLivre/controller/CadastroController.java
@@ -3,7 +3,9 @@ package br.com.aceleradora.RegistroLivre.controller;
 import br.com.aceleradora.RegistroLivre.model.Empresa;
 import br.com.aceleradora.RegistroLivre.model.EntidadeDAO;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
 
+@Resource
 public class CadastroController {
 	
 	//private EmpresaDAO dao;
