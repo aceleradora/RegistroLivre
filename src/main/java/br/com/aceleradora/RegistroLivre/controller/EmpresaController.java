51f4e96d12229880d7b76341e2109ec77234b960
Henrique Braga <hbraga.santos@gmail.com>
2014-11-07 15:52:53 -0200

    4: Joseane e Henrique - Tirando comentários e arrumando nomes na classe EmpresaController!

--- src/main/java/br/com/aceleradora/RegistroLivre/controller/EmpresaController.java
+++ src/main/java/br/com/aceleradora/RegistroLivre/controller/EmpresaController.java
@@ -1,17 +1,22 @@
 package br.com.aceleradora.RegistroLivre.controller;
 
+import br.com.aceleradora.RegistroLivre.dao.EmpresaDAO;
 import br.com.aceleradora.RegistroLivre.model.Empresa;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 @Resource
 public class EmpresaController {
 	
-	//private EmpresaDAO dao;
-
-//	public CadastroController(EmpresaDAO dao){
-//		this.dao = dao;
-//	}
+	private EmpresaDAO daoEmpresa;
+	private Result result;
+	
+	public EmpresaController(EmpresaDAO dao, Result result){
+		
+		this.result = result;
+		this.daoEmpresa = dao;
+	}
 	
 	@Get("/cadastro")
 	public void cadastro() {
@@ -23,8 +28,8 @@ public class EmpresaController {
 		
 	}
 	
-//	public void cadastrar(Empresa empresa){
-//		EmpresaDAO.adiciona(empresa);
-//	}
+	public void cadastrar(Empresa empresa){
+		daoEmpresa.adiciona(empresa);
+	}
 	
 }
