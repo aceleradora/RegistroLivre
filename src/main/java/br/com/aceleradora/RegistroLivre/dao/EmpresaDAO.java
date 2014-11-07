321b72f1a2d45e68efc9c5be2e16c96827d0217a
Lucas Rangel <lrcezimbra@hotmail.com>
2014-11-07 15:23:40 -0200

    1 - Rangel/Yasser - Interface IEmpresaDAO adicionada.

--- src/main/java/br/com/aceleradora/RegistroLivre/dao/EmpresaDAO.java
+++ src/main/java/br/com/aceleradora/RegistroLivre/dao/EmpresaDAO.java
@@ -2,14 +2,14 @@ package br.com.aceleradora.RegistroLivre.dao;
 
 import java.util.List;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import br.com.aceleradora.RegistroLivre.model.Empresa;
 import br.com.caelum.vraptor.ioc.Component;
 
 @Component
-public class EmpresaDAO {
+public class EmpresaDAO implements IEmpresaDAO{
 	
 	private Session sessao;
 	
@@ -21,12 +21,19 @@ public class EmpresaDAO {
 		return sessao.createQuery("FROM Empresa").list();		
 	}
 	
-	public Empresa getEmpresaById(long id){
+	public Empresa getById(long id){
 		try{
 			Empresa empresa = (Empresa) sessao.get(Empresa.class, id);
 			return empresa;
 		}catch(Exception e){
 			return null;
 		}
+	}
+
+	@Override
+	public void adiciona(Empresa empresa) {		
+		Transaction transacao = sessao.beginTransaction();
+		sessao.save(empresa);
+		transacao.commit();
 	}
 }
