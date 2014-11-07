4f706880f3a5034ba2d4c2d5536c96960759cc42
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-07 13:43:54 -0200

    #1:<Jordani/Everton>-update CadastroController

--- src/main/java/br/com/aceleradora/RegistroLivre/controller/CadastroController.java
+++ src/main/java/br/com/aceleradora/RegistroLivre/controller/CadastroController.java
@@ -1,12 +1,24 @@
 package br.com.aceleradora.RegistroLivre.controller;
 
+import br.com.aceleradora.RegistroLivre.model.Empresa;
+import br.com.aceleradora.RegistroLivre.model.EntidadeDAO;
 import br.com.caelum.vraptor.Get;
 
 public class CadastroController {
+	
+	//private EmpresaDAO dao;
+
+//	public CadastroController(EmpresaDAO dao){
+//		this.dao = dao;
+//	}
 	
 	@Get("/cadastro")
 	public void cadastro() {
 		
 	}
-
+	
+//	public void cadastrar(Empresa empresa){
+//		EmpresaDAO.adiciona(empresa);
+//	}
+	
 }
