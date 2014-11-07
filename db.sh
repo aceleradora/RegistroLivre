2576da2dfab0f8bba0b787f82da6ae49b7c3053a
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-07 11:13:40 -0200

    Criado package de teste do cadastroController

--- db.sh
+++ db.sh
@@ -7,9 +7,8 @@ sudo apt-get install maven
 
 echo 'export DB_CONNECTION_URL="jdbc:postgresql://localhost/registro_livre"' >> ~/.bashrc
 
-source ~/.bashrc
-
 sudo -u postgres psql -c "CREATE DATABASE registro_livre"
 
 sudo -u postgres psql -c "CREATE USER mezxofwrhzixqp WITH PASSWORD 'U3wNSZNZ7rdOW4BVvw7x2ydYZb'"
 
+exec bash
---
+++ src/main/java/br/com/aceleradora/RegistroLivre/test/CadastroControllerTest.java
@@ -0,0 +1,14 @@
+package br.com.aceleradora.RegistroLivre.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadastroControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
