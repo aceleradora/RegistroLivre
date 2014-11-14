a2d89abc4400900bf3909f060ca871ba4f529cb1
apdaros <anapauladaros@gmail.com>
2014-11-14 14:46:52 -0200

    #SQ:Carisa, Lucas - Criando teste funcional com Fluentlenium + HTMLUnitDriver

---
+++ src/test/java/funcional/SeleniumTest.java
@@ -0,0 +1,42 @@
+package funcional;
+
+import java.io.File;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SeleniumTest extends FluentTest {
+	private static final String URL_PAGE = "http://registro-livre-aceleradora.herokuapp.com";
+	
+	protected static String path;
+		
+	static {
+		path = new File("").getAbsolutePath()+"/src/main/resources";
+		System.setProperty("webdriver.chrome.driver", path+"/chromedriver");
+	}
+	
+	public static WebDriver driver = new HtmlUnitDriver();
+	
+	@Override
+	public WebDriver getDefaultDriver() {
+		return driver;
+	}
+	
+	@Test
+	public void deveTestarOIndex(){
+		
+		goTo(URL_PAGE);
+		 
+		assertThat(title()).contains("Registro Livre");
+	}
+	
+	
+}
