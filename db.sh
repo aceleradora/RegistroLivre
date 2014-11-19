03c95d413d85176225fdc077037cca8489f866d2
Lucas Rangel <lrcezimbra@hotmail.com>
2014-11-19 15:32:49 -0200

    #00: <Viola,Yasser> - Adicionando variáveis do cloudinary no script de automatização do ambiente.

--- db.sh
+++ db.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 ##Script de instalação
+
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 
 sudo apt-get install maven
 
-echo 'Digite login do banco'
-read usuario
+if [ -z "$DB_CONNECTION_USUARIO" ] && [ -z "$DB_CONNECTION_SENHA"]; then
+	echo '[DIGITE O USUARIO DO BANCO]:'
+	read usuario
+	echo "export DB_CONNECTION_USUARIO=$usuario" >> ~/.bashrc
 
-unset DB_CONNECTION_USUARIO
-echo "export DB_CONNECTION_USUARIO=$usuario" >> ~/.bashrc
+	echo '[DIGITE A SENHA DO BANCO]:'
+	read senha
+	echo "export DB_CONNECTION_SENHA=$senha">> ~/.bashrc
+
+	sudo -u postgres psql -c "CREATE USER $usuario WITH PASSWORD '$senha'"
+fi
+
+if [ -z "$DB_CONNECTION_URL" ]; then
+	echo 'export DB_CONNECTION_URL="jdbc:postgresql://localhost/registro_livre"' >> ~/.bashrc
+fi
 
 
-echo 'Digite senha do banco'
-read senha
-unset DB_CONNECTION_SENHA
-echo "export DB_CONNECTION_SENHA=$senha" >> ~/.bashrc	
+if [ -z "$API_SECRET" ]; then
+	echo '[DIGITE O SECRET DO CLOUDINARY]:'
+	read api_secret
+	echo "export API_SECRET=$api_secret" >> ~/.bashrc
+fi
 
-unset DB_CONNECTION_URL
-echo 'export DB_CONNECTION_URL="jdbc:postgresql://localhost/registro_livre"' >> ~/.bashrc
+if [ -z  "$API_KEY" ]; then
+	echo '[DIGITE O API KEY DO CLODUINARY]:'
+	read api_key
+	echo "export API_SECRET=$api_key" >> ~/.bashrc
+fi
 
-sudo -u postgres psql -c "CREATE DATABASE registro_livre"
-
-sudo -u postgres psql -c "CREATE USER $usuario WITH PASSWORD '$senha'"
+if [ -z "$CLOUD_NAME" ]; then
+	echo '[DIGITE O CLOUD NAME DO CLOUDINARY]:'
+	read cloud_name
+	echo "export CLOUD_NAME=$cloud_name">>~/.bashrc
+fi
 
 exec bash
