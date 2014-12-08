3039a43bb682a0857174518a9385bb507bcb0e4f
Lucas Rangel <lrcezimbra@hotmail.com>
2014-12-08 15:26:54 -0200

    #00: <Alexandre, Jordani, Rangel> - Retirados metodos desnecessarios do HomeController

--- src/main/java/br/com/aceleradora/RegistroLivre/controller/HomeController.java
+++ src/main/java/br/com/aceleradora/RegistroLivre/controller/HomeController.java
@@ -1,10 +1,6 @@
 package br.com.aceleradora.RegistroLivre.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.aceleradora.RegistroLivre.dao.EmpresaDAO;
-import br.com.aceleradora.RegistroLivre.model.Empresa;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
@@ -22,18 +18,5 @@ public class HomeController {
 
 	@Get("/")
 	public void home() {
-	}
-
-	public List<Empresa> buscaPorNomeFantasia(String nomeFantasiaRequerido) {
-		List<Empresa> listaDeEmpresas = new ArrayList<Empresa>();
-
-		for (Empresa empresa : daoEmpresa
-				.pesquisaPorNomeFantasia(nomeFantasiaRequerido)) {
-			if (empresa.getNomeFantasia().equals(nomeFantasiaRequerido)) {
-				listaDeEmpresas.add(empresa);
-			}
-		}
-
-		return listaDeEmpresas;
 	}
 }
