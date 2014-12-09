049b2b06197bf870dbd37addb5e98ee4bb5d1b51
Henrique Braga <hbraga.santos@gmail.com>
2014-12-09 13:43:16 -0200

    #0: <Henrique, Viola> - Criando testes funcionais da Busca

---
+++ src/test/java/funcional/BuscaTest.java
@@ -0,0 +1,39 @@
+package funcional;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import br.com.caelum.vraptor.restfulie.relation.RelationBuilder.WithName;
+
+public class BuscaTest extends FluentTest{
+
+	private static final String URL_PAGE = "http://localhost:8080";
+	
+	public static WebDriver driver = new HtmlUnitDriver();
+	
+	@Override
+	public WebDriver getDefaultDriver() {
+		return driver;
+	}
+	
+	@Test
+	public void quandoClicaNoBotãoSemEnviarNadaDeveRetornarErroParaPáginaPrincipal(){
+		
+		goTo(URL_PAGE);
+		 
+		find("#btn-submit").click();
+		
+		assertThat(find(".alert").getText(), containsString("Não há nenhum registro de empresa para a busca efetuada."));
+	
+//		find(".alert").contains("delicia de teste");
+	}
+	
+	
+	
+}
