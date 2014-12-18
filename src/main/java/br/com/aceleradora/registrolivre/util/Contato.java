1e72c3f699507b1a58173c90dcc97fa16b2a318b
Lucas Rangel <lrcezimbra@hotmail.com>
2014-12-18 13:43:11 -0200

    #13: <Ana, Yasser> - Testando email Hostinger

--- src/main/java/br/com/aceleradora/registrolivre/util/Contato.java
+++ src/main/java/br/com/aceleradora/registrolivre/util/Contato.java
@@ -55,7 +55,7 @@ public class Contato {
 				new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() 
 			{
-				return new PasswordAuthentication("registrolivreaceleradora@gmail.com", System.getenv("SENHA_EMAIL"));
+				return new PasswordAuthentication("contato@registrolivre.hol.es", System.getenv("SENHA_EMAIL"));
 			}
 		});
 
