d27073e53a4540810ccde462fcb13c3218af6821
Lucas Rangel <lrcezimbra@hotmail.com>
2015-01-06 13:46:17 -0200

    #00: <Rangel, Yasser> - Adicionados testes do validador de CNPJ

--- src/test/java/unitario/validador/CNPJValidadorTest.java
+++ src/test/java/unitario/validador/CNPJValidadorTest.java
@@ -1,5 +1,46 @@
 package unitario.validador;
 
-public class CNPJValidadorTest {
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+import static unitario.validador.HibernateValidationUtils.*;
+
+import br.com.aceleradora.registrolivre.model.Empresa;
+
+public class CNPJValidadorTest {
+	public Empresa empresa;
+
+	@Before
+	public void setup() {
+		empresa = new Empresa();
+	}
+
+	@Test
+	public void sucessoSeOCnpjEValido() {
+		empresa.setCnpj("47960950/0449-27");
+		
+		assertThat(validacaoPara(empresa, noCampo("cnpj")), sucesso());
+	}
+
+	@Test
+	public void falhaSeOCnpjForInvalido() {
+		empresa.setCnpj("11111111/1111-11");
+		
+		assertThat(validacaoPara(empresa, noCampo("cnpj")), falha());
+	}
+
+	@Test
+	public void falhaSeOCnpjForLetras() {
+		empresa.setCnpj("aaaa");
+		
+		assertThat(validacaoPara(empresa, noCampo("cnpj")), falha());
+	}
+
+	@Test
+	public void falhaSeOCnpjForNulo() {
+		empresa.setCnpj(null);
+		
+		assertThat(validacaoPara(empresa, noCampo("cnpj")), falha());
+	}
 }
