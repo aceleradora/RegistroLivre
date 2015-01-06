7be33e746af69b1b45da006ab95e23b986c03475
Lucas Rangel <lrcezimbra@hotmail.com>
2015-01-06 11:24:47 -0200

    #00: <Rangel, Yasser> - Adicionado testes para validação por annotation de NomeFantasia e CPF

---
+++ src/test/java/validador/CPFValidadorTest.java
@@ -0,0 +1,41 @@
+package validador;
+
+import static org.junit.Assert.assertThat;
+import static validador.HibernateValidationUtils.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.aceleradora.registrolivre.model.Socio;
+
+public class CPFValidadorTest {
+	
+	private Socio socio;
+	
+	@Before
+	public void setUp(){
+		socio = new Socio();
+	}
+		
+	@Test
+	public void sucedeSeCpfEValido(){
+		socio.setCpf("083.533.674-32");
+		
+		assertThat(validacaoPara(socio, noCampo("cpf")), sucesso());
+	}
+	
+	@Test	
+	public void falhaSeCpfForInvalidoForLetras(){
+		socio.setCpf("aaa");
+		
+		assertThat(validacaoPara(socio, noCampo("cpf")), falha());
+	}
+	
+	@Test	
+	public void falhaSeCpfForInvalido(){
+		socio.setCpf("111.111.111-11");
+
+		assertThat(validacaoPara(socio, noCampo("cpf")), falha());
+	}
+
+}
---
+++ src/test/java/validador/NomeFantasiaValidadorTest.java
@@ -0,0 +1,43 @@
+package validador;
+
+import static org.junit.Assert.assertThat;
+import static validador.HibernateValidationUtils.noCampo;
+import static validador.HibernateValidationUtils.sucesso;
+import static validador.HibernateValidationUtils.falha;
+import static validador.HibernateValidationUtils.validacaoPara;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.aceleradora.registrolivre.model.Empresa;
+
+public class NomeFantasiaValidadorTest {
+
+	private Empresa empresa;
+
+	@Before
+	public void setUp() {
+		empresa = new Empresa();
+	}
+
+	@Test
+	public void sucedeSeONomeFantasiaTiverMaisQueUmCaracter() {
+		empresa.setNomeFantasia("aaa");
+
+		assertThat(validacaoPara(empresa, noCampo("nomeFantasia")), sucesso());
+	}
+
+	@Test
+	public void falhaSeONomeFantasiaTiverMenosQueDoisCaracteres() {
+		empresa.setNomeFantasia("A");
+
+		assertThat(validacaoPara(empresa, noCampo("nomeFantasia")), falha());
+	}
+
+	@Test
+	public void falhaSeONomeFantasiaForNulo() {
+		empresa.setNomeFantasia(null);
+
+		assertThat(validacaoPara(empresa, noCampo("nomeFantasia")), falha());
+	}
+}
