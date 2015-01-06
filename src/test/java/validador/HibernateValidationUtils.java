5bb7ecf92cdf05cfc2445adec2b6a59a5586a5dc
Lucas Rangel <lrcezimbra@hotmail.com>
2015-01-06 11:24:00 -0200

    #00: <Rangel, Yasser> - Adicionada classe util para testar as validações por annotation

---
+++ src/test/java/validador/HibernateValidationUtils.java
@@ -0,0 +1,55 @@
+package validador;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.mockito.internal.matchers.Not;
+
+public class HibernateValidationUtils {
+	private static Validator VALIDADOR;
+
+	static {
+		ValidatorFactory fabrica = Validation.buildDefaultValidatorFactory();
+		VALIDADOR = fabrica.getValidator();
+	}
+
+	public static Set<ConstraintViolation<Object>> validacaoPara(Object objeto,
+			String nomeCampo) {
+		return VALIDADOR.validateProperty(objeto, nomeCampo);
+	}
+
+	public static String noCampo(String nomeCampo) {
+		return nomeCampo;
+	}
+
+	public static Matcher<Set<ConstraintViolation<Object>>> sucesso() {
+		return new PassaValidacao();
+	}
+
+	public static Matcher<Set<ConstraintViolation<Object>>> falha() {
+		return new Not(new PassaValidacao());
+	}
+
+	static class PassaValidacao extends	BaseMatcher<Set<ConstraintViolation<Object>>> {
+		@Override
+		public boolean matches(Object o) {
+			boolean resultado = false;
+			if (o instanceof Set) {
+				resultado = ((Set) o).isEmpty();
+			}
+			return resultado;
+		}
+
+		@Override
+		public void describeTo(Description descricao) {
+			descricao.appendText("valid");
+		}
+	}
+}
