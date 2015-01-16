804429bf9c597aaee93d21e83afa8896b5e9f87a
= <anuar.yasser@gmail.com>
2015-01-16 16:25:25 -0200

    #00: <Rangel, Yasser> - Refatorando métodos adicionaSocios() e adicionaSocioComDados(), aplicando namespaces

--- src/main/webapp/assets/js/adiciona-socios.js
+++ src/main/webapp/assets/js/adiciona-socios.js
@@ -1,10 +1,15 @@
-var contSocios = 1;
+var RegistroLivre = RegistroLivre || {};
 
-function adicionaSocio(){
-	var htmlSocio = 
-		'<li class="list-group-item socio-group" id="socio' + contSocios + '">' +
+RegistroLivre.Socios = function Socios(){
+	var $template = 
+		$('<li class="list-group-item socio-group">' +
 			'<div class="list-group-item-heading centralize">' +
-				'<h4>Dados do sócio <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></h4>' +
+				'<h4>Dados do sócio' +
+					'<button type="button" class="close" data-dismiss="alert">' +
+					    '<span aria-hidden="true">&times;</span>' +
+						'<span class="sr-only">Close</span>' +
+					 '</button>' +
+		         '</h4>' +
 			'</div>' +
 			'<div class="form-group">' +
 				'<label>Nome do sócio</label>' +
@@ -20,43 +25,23 @@ function adicionaSocio(){
 					'<input type="checkbox" name="empresa.socios[].ativo" checked> Ativo' +
 				'</label>' + 
 			'</div>' +
-		'</li>';
-
-	$("#divSocios").append(htmlSocio);
-	$('.cpf').mask('000.000.000-00');
-	contSocios++;
-} 
-
-function adicionaSociosCadastrados(nome, cpf, ativo){
+		'</li>');
 	
-	if(ativo=="true"){		
-		botaoHtmlAtivo = '<input type="checkbox" name="empresa.socios[].ativo" checked> Ativo' ;
-	} else {		
-		botaoHtmlAtivo = '<input type="checkbox" name="empresa.socios[].ativo"> Ativo' ;
+	
+	var adicionaSocio = function adicionaSocio(){
+		$template.find('.cpf').mask('000.000.000-00');
+		$("#divSocios").append($template);
+	}
+		
+	var adicionaSocioComDados = function adicionaSocioComDados(nome, cpf, ativo){
+		$template.find("input[name='empresa.socios[].nome']").val(nome);
+		$template.find("input[name='empresa.socios[].cpf']").val(cpf);
+		$template.find("input[type=checkbox]").attr("checked", ativo);
+		adicionaSocio();
 	}
 	
-	var htmlSocioCadastrados = 
-		'<li class="list-group-item socio-group" id="socio' + contSocios + '">' +
-			'<div class="list-group-item-heading centralize">' +
-				'<h4>Dados dos sócios <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></h4>' +
-			'</div>' +
-			'<div class="form-group">' +
-				'<label>Nome do sócio</label>' +
-				'<input class="form-control nome-socio" name="empresa.socios[].nome" value ="'+nome+'" required>' +
-			'</div>' +
-			'<div class="form-group has-feedback cpf-group">' +
-				'<label>CPF</label>' +
-				'<input class="form-control cpf" name="empresa.socios[].cpf" value ="'+cpf+'" placeholder="ex: 000.000.000-00" required>' +
-				'<span class="glyphicon form-control-feedback"></span>' +
-			'</div>' +
-			'<div class="form-group">' +
-				'<label>' + 
-					botaoHtmlAtivo +
-				'</label>' + 
-			'</div>' +
-		'</li>';
-
-	$("#divSocios").append(htmlSocioCadastrados);
-	$('.cpf').mask('000.000.000-00');
-	contSocios++;
-} 
+	return {
+		adicionaSocio: adicionaSocio,
+		adicionaSocioComDados: adicionaSocioComDados
+	}
+}
