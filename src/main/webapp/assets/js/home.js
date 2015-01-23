08f999b6c1f352d4caf72650d7803b24e393ad68
othman853 <anuar.yasser@gmail.com>
2015-01-23 11:01:36 -0200

    #00: <Joseane, Yasser> - Refatorando o evento de click do link da busca avançada

--- src/main/webapp/assets/js/home.js
+++ src/main/webapp/assets/js/home.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
 	
 	retiraCampoBuscaNavbar();
 	eventoDesbloquearBotaoPesquisar();
-	
 	validarBuscaAvancada();
+	
+	$("#abrir-busca-avancada").click(function(e){
+		e.preventDefault();		
+		
+		mostraBuscaAvancada();
+		animacoes.rolaLinksAncoraDeFormaLenta($("#busca-avancada"));		
+	});
+	
 });
 
-function retiraCampoBuscaNavbar(){
+function retiraCampoBuscaNavbar() {
 	var tagRegistroLivre = $("#registrolivre");
 	if (tagRegistroLivre) {
 		$("#inputaBuscaNavbar").hide();
@@ -26,80 +33,69 @@ function fechaBuscaAvancada() {
 	$("#busca-avancada").fadeOut();
 }
 
-function eventoDesbloquearBotaoPesquisar(){
+function eventoDesbloquearBotaoPesquisar() {
 	$("#campoPesquisado").keyup(function() {
 		var busca = $("#campoPesquisado").val();
-		if (busca.length >= 2) {                  
+		if (busca.length >= 2) {
 			habilita($("#btn-submit"));
-		} else {                                   
+		} else {
 			desabilita($("#btn-submit"));
 		}
 	});
 }
 
-var rolaLinks = function rolaLinks() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 800);
-          return false;
-        }
-      }
-    };
+function contaCamposPreenchidos(formulario) {
+	var contador = 0;
+	formulario.find("input[type='text']").each(function() {
+		if ($(this).val().length != 0 && $(this).val() != ' ') {
 
-function bindEvents() {
-  $('a[href*=#]:not([href=#])').click(rolaLinks);
-}
-
-function contaCamposPreenchidos(formulario){
-	var contador = 0;	
-	formulario.find("input[type='text']").each(function(){
-		if($(this).val().length != 0 && $(this).val() != ' '){
-			contador++;
 		}
-	});	
-	return contador;	
+	});
+	return contador;
 }
 
-function habilita(botao){
+function habilita(botao) {
 	botao.prop("disabled", false);
 }
 
-function desabilita(botao){
+function desabilita(botao) {
 	botao.prop("disabled", true);
 }
 
 function validarBuscaAvancada() {
 	var formulario = $("#pesquisa-avancada");
 	var camposDeTexto = formulario.find("input[type='text']");
-	var estados = $("#estado");	
+	var estados = $("#estado");
 	var estadoSelecionado = estados.find(":selected");
 	var botao = $("#botao-pesquisa-avancada");
-		
-	camposDeTexto.each(function() {
-		$(this).keyup(function() {
-			estadoSelecionado = $("#estado").find(":selected");			
-			if($(this).val().length == 0){
-				if(contaCamposPreenchidos($("#pesquisa-avancada")) == 0 && estadoSelecionado.val() == ''){
-					desabilita(botao);
-				}else{
-					habilita(botao);
-				}				
-			}else{				
-				habilita(botao);
-			}			
-		});
-	});
-	
-	estados.change(function(){
+
+	camposDeTexto
+			.each(function() {
+				$(this)
+						.keyup(
+								function() {
+									estadoSelecionado = $("#estado").find(
+											":selected");
+									if ($(this).val().length == 0) {
+										if (contaCamposPreenchidos($("#pesquisa-avancada")) == 0
+												&& estadoSelecionado.val() == '') {
+											desabilita(botao);
+										} else {
+											habilita(botao);
+										}
+									} else {
+										habilita(botao);
+									}
+								});
+			});
+
+	estados.change(function() {
 		console.log("Change: " + $(this).find(":selected").val());
-		if(contaCamposPreenchidos(formulario) == 0 && $(this).find(":selected").val() == ''){
+		if (contaCamposPreenchidos(formulario) == 0
+				&& $(this).find(":selected").val() == '') {
 			desabilita(botao);
-		}else{
+		} else {
 			habilita(botao);
-		}				
+		}
 	});
 }
