82ac2936a86e59070197d8ce13abeaf21f2904cb
othman853 <anuar.yasser@gmail.com>
2015-02-04 10:19:45 -0200

    #00: <Viola, Yasser> - Alterando texto padrão de selects de cidade e estado

--- src/main/webapp/assets/js/cidades-estados-1.2-utf8.js
+++ src/main/webapp/assets/js/cidades-estados-1.2-utf8.js
@@ -103,7 +103,7 @@ dgCidadesEstados.prototype = {
     var opts = this.cidade;
     while (opts.childNodes.length) opts.removeChild(opts.firstChild); // limpa a lista atual
 	
-    this.addOption(opts, '', 'Selecione uma cidade');
+    this.addOption(opts, '', 'Cidade...');
     for (var i=0;i<itens_total;i++) this.addOption(opts, itens[i], itens[i]); // vai adicionando as cidades correspondentes
   },
   addOption: function (elm, val, text) {
@@ -113,7 +113,7 @@ dgCidadesEstados.prototype = {
     elm.appendChild(opt);
   },
   estados : [
-    ['','Selecione um estado'],['AC','Acre'],['AL','Alagoas'],['AM','Amazonas'],['AP','Amapá'],['BA','Bahia'],
+    ['','Estado...'],['AC','Acre'],['AL','Alagoas'],['AM','Amazonas'],['AP','Amapá'],['BA','Bahia'],
     ['CE','Ceará'],['DF','Distrito Federal'],['ES','Espírito Santo'],['GO','Goiás'],['MA','Maranhão'],['MG','Minas Gerais'],
     ['MS','Mato Grosso do Sul'],['MT','Mato Grosso'],['PA','Pará'],['PB','Paraíba'],['PE','Pernambuco'],['PI','Piauí'],
     ['PR','Paraná'],['RJ','Rio de Janeiro'],['RN','Rio Grande do Norte'],['RO','Rondônia'],['RR','Roraima'],['RS','Rio Grande do Sul'],
