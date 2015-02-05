d1fd2d0c8667b38de151efa1c5af604fbe6a4923
othman853 <anuar.yasser@gmail.com>
2015-02-05 10:25:44 -0200

    #00:<Joseane, Yasser> - Alterando texto padrão de checkboxes

--- src/main/webapp/assets/js/cidades-estados-1.2-utf8.js
+++ src/main/webapp/assets/js/cidades-estados-1.2-utf8.js
@@ -113,7 +113,7 @@ dgCidadesEstados.prototype = {
     elm.appendChild(opt);
   },
   estados : [
-    ['','Estado...'],['AC','Acre'],['AL','Alagoas'],['AM','Amazonas'],['AP','Amapá'],['BA','Bahia'],
+    ['','Selecione...'],['AC','Acre'],['AL','Alagoas'],['AM','Amazonas'],['AP','Amapá'],['BA','Bahia'],
     ['CE','Ceará'],['DF','Distrito Federal'],['ES','Espírito Santo'],['GO','Goiás'],['MA','Maranhão'],['MG','Minas Gerais'],
     ['MS','Mato Grosso do Sul'],['MT','Mato Grosso'],['PA','Pará'],['PB','Paraíba'],['PE','Pernambuco'],['PI','Piauí'],
     ['PR','Paraná'],['RJ','Rio de Janeiro'],['RN','Rio Grande do Norte'],['RO','Rondônia'],['RR','Roraima'],['RS','Rio Grande do Sul'],
