56e04947fa716536ab05584789d21a7d2bf657e8
msulista <marcus.rodrigues81@gmail.com>
2015-02-10 14:07:28 -0200

    #00: <Marcus, Rangel> - Retirando scripts não utilizados.

--- src/main/webapp/assets/js/canvasloader-loader.js
+++
@@ -1,8 +0,0 @@
-var cl = new CanvasLoader('canvasloader-container');
-cl.setColor('#2c3e50'); // default is '#000000'
-cl.setShape('roundRect'); // default is 'oval'
-cl.setDiameter(86); // default is 40
-cl.setDensity(70); // default is 40
-cl.setRange(0.7); // default is 1.3
-cl.setSpeed(4); // default is 2
-cl.setFPS(27);
