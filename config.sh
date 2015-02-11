6b6691253571c22eb6e5a1d18a12b5d8b84b73ce
lucasviola <lucas_jjjj@hotmail.com>
2015-02-11 15:34:11 -0200

    #24: <Jordani, Viola> - Criando script de configuração config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-##Script de instalação
 
+## Script de instalação do projeto Registro Livre
+## Repositório oficial do projeto: https://github.com/aceleradora6-tw/RegistroLivre
+
+## Instalando o postgreSQL
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 
+## Configura o Postgres para tirar os acentos dos dados.
 sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS unaccent;"
 
+## Isntalando o Maven
 sudo apt-get install maven
 
+## Configurando o postgreSQL
 if [ -z "$DB_CONNECTION_USUARIO" ] && [ -z "$DB_CONNECTION_SENHA"]; then
 	echo '[DIGITE O USUARIO DO BANCO]:'
 	read usuario
@@ -24,7 +30,8 @@ if [ -z "$DB_CONNECTION_URL" ]; then
 	echo 'export DB_CONNECTION_URL="jdbc:postgresql://localhost/registro_livre"' >> ~/.bashrc
 fi
 
-
+## Configurando as informações do Cloudinary
+## Caso não saiba o que são essas informações, leia o README do projeto.
 if [ -z "$API_SECRET" ]; then
 	echo '[DIGITE O SECRET DO CLOUDINARY]:'
 	read api_secret
@@ -32,7 +39,7 @@ if [ -z "$API_SECRET" ]; then
 fi
 
 if [ -z  "$API_KEY" ]; then
-	echo '[DIGITE O API KEY DO CLODUINARY]:'
+	echo '[DIGITE O API KEY DO CLOUDINARY]:'
 	read api_key
 	echo "export API_KEY=$api_key" >> ~/.bashrc
 fi
@@ -47,6 +54,15 @@ if [ -z "$SENHA_EMAIL" ]; then
 	echo '[DIGITE A SENHA PARA O EMAIL DE CONTATO]:'
 	read senha_email
 	echo "export SENHA_EMAIL=$senha_email">>~/.bashrc
+fi
+
+## Configurando as variáveis de ambiente do Selenium
+if [ -z "$WEBDRIVER_PATH" ]; then
+	echo "export WEBDRIVER_PATH=$PWD/src/main/resources/chromedriver">>~/.bashrc
+fi
+
+if [ -z "$URL_ENV_TESTE" ]; then
+	echo "export URL_ENV_TESTE=http://localhost:8080">>~/.bashrc
 fi
 
 exec bash
